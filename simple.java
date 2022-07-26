96cd646b2f5697a2d8cafe743ba3694f06c56227
edureka <support@edureka.co>
2022-07-26 12:23:06 +0000

    java script change back to the original

--- simple.java
+++ simple.java
@@ -1,5 +1,5 @@
-public class simple{
-  public static void main(string args[]){
-    system.out.println("Hello git, Welcome to Online Training");
+ class simple{
+     public static void main(string args[]){
+     system.out.println("Hello git, Welcome to Online Training");
     }
 }
